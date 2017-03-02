fee4a60e9cde59a6d5be9a039105c7686d7aa548
Wei-Tsung Lin <fasthall@gmail.com>
2017-03-01 16:48:45 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pihook
+
+A simple webserver that works as webhook. After triggered, it pulls source code and compile it, then uploads it to Raspberry Pi.
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"net/http"
+
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+var pi string
+var repo string
+
+type PushEvent struct {
+	Pusher     *Pusher     `json:"pusher" binding:"required"`
+	Repository *Repository `json:"repository" binding:"required"`
+	HeadCommit *HeadCommit `json:"head_commit" binding:"required"`
+}
+
+type Pusher struct {
+	Name  string `json:"name" binding:"required"`
+	Email string `json:"email" binding:"required"`
+}
+
+type Repository struct {
+	Name  string `json:"name" binding:"required"`
+	Owner *Owner `json:"owner" binding:"required"`
+}
+
+type Owner struct {
+	Name string `json:"name" binding:"required"`
+}
+
+type HeadCommit struct {
+	ID string `json:"id" binding:"required"`
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/repo", getRepo)
+	router.POST("/repo", postRepo)
+	router.GET("/pi", getPi)
+	router.POST("/pi", postPi)
+	router.POST("/webhook", webhook)
+	router.Run()
+}
+
+func getRepo(c *gin.Context) {
+	c.String(http.StatusOK, repo)
+}
+
+func postRepo(c *gin.Context) {
+	repo = c.Query("host")
+	c.String(http.StatusOK, c.Query("host")+" added\n")
+}
+
+func getPi(c *gin.Context) {
+	c.String(http.StatusOK, pi)
+}
+
+func postPi(c *gin.Context) {
+	pi = c.Query("host")
+	c.String(http.StatusOK, c.Query("host")+" added\n")
+}
+
+func webhook(c *gin.Context) {
+	var push PushEvent
+	c.Bind(&push)
+	fmt.Println(push)
+	c.String(http.StatusOK, "OK")
+}
